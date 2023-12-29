f1ee5e6a83acc6d9e1f07aa33380f4ac74d9e8e6
AM4SO <79862724+AM4SO@users.noreply.github.com>
2023-12-29 22:37:20 +0000

    update

--- mlProjects/Connect-4/.ipynb_checkpoints/game-checkpoint.py
+++ mlProjects/Connect-4/.ipynb_checkpoints/game-checkpoint.py
@@ -1,27 +1,7 @@
 import numpy as np
 
 class game:
-  """
-  A class for a customisable connect-4 game.
-
-  
-  Methods:
-    getState\n
-    show\n
-    reset\n
-    step
-
-    
-  Attributes:
-    done, rows, cols, nodesToWin, gameboard, state, winSlice
-
-  Play the game by calling game.step(), whilst switching between the players.
-  Repeat this until game.done == True. At this point, a player must have won
-  or a player must have tried to play after the board was already filled. Call
-  game.step once more for the other player, so that both players have been 
-  told that the game has ended. 
-  """
-  def __init__(self, rows:int, cols:int, nodesToWin = 4):
+  def __init__(self, rows, cols, nodesToWin = 4):
     """
     Constructor(rows, columns, nodesToWin=4)
     """
@@ -30,44 +10,31 @@ class game:
     self.cols = cols
     self.nodesToWin = nodesToWin
     self.gameboard = -np.ones((rows, cols),dtype=np.int8)
-    self.winSlice = [[0]*nodesToWin,[1]*nodesToWin]
+    self.winSlice = [[],[]]
     self.state = np.zeros((2, 4*rows,cols,1),dtype = np.int8)#rowSet0 = empty, rowSet1=localPlr, rowSet2=OtherPlayer, rowSet4=AnythingThere. 1 if plr0 or plr1
-    self.winner = -1
-
-    self.winReward = 100
-    self.loseReward = -100
-    self.incorrectInputReward = -1
-    self.drawReward = 0
-    self.normalReward = 0
+    for i in range(nodesToWin):
+      self.winSlice[0].append(0)
+      self.winSlice[1].append(1)
   
-  def peekActions(self, actions:np.ndarray, plr:int) -> np.ndarray:
-    """Returns the states that follow each given action"""
-    ret = np.tile(self.gameboard, (len(actions), 1, 1) )
-    legalMoves = self.getLegalMoves()
-    for i, action in enumerate(actions):
-      assert action in legalMoves, f"Cannot peek illegal action {action}"
-
-      ret[i, self.findBottomFree(action), action] = plr
-    return ret
-
-  def getLegalMoves(self):
-    ret = np.where(self.gameboard[0,:] == -1)[0]
-    if len(ret) == 0:
-      return np.array([-1], dtype = np.intp)
-    return ret
     
   def checkPos(self, pos):
-    """
-    checkPos((X, Y))
-    Returns true if the given position is empty
-    """
+      """
+      checkPos((X, Y))
+      Returns true if the given position is empty
+      """
     if self.gameboard[pos[0],pos[1]] == -1:
       return True
     return False
 
   def getCol(self, pos):
-    return self.gameboard[:, pos].copy()
-  
+    col = []
+    for row in range(self.rows):
+      col.append(self.gameboard[row, pos])
+    return col
+  def getCoords(self, pos):
+    col = self.getCol(pos)
+    row = -1
+    return [col, row]
   def getDiags(self, loc):
     row, col = loc
     c = row-col#C in formula of diag y=mx+c
@@ -87,7 +54,7 @@ class game:
         diag1.append(self.gameboard[row,col])
     return [diag, diag1]
 
-  def checkWin(self, plr:int, placed:tuple):
+  def checkWin(self, plr, placed):
     row = self.gameboard[placed[0]]
     col = self.getCol(placed[1])
     win = self.winSlice[plr]
@@ -100,44 +67,45 @@ class game:
       return True
     return False
   
-  def step(self, action:int, plr:int) -> tuple["state", int, bool]:
+  def step(self, action, plr):
     if self.done:#other player won on their last turn
-      return (self.getState(plr), self.loseReward, True)
+        return (self.getState(plr), -1, True)
     x = self.placeAt(action, plr)
     if x == -10:
-        return (self.getState(plr), self.incorrectInputReward, False)
+        return (self.getState(plr), -0.3, False)
     if x == -1:
-        return (self.getState(plr), self.normalReward, False)
+        return (self.getState(plr), 0, False)
     if x == plr:
-        self.winner = plr
-        return (self.getState(plr), self.winReward, True)
+        return (self.getState(plr), 1, True)
     if x == -100:
-        return (self.getState(plr), self.drawReward, True)
-    
-  def findBottomFree(self, column:int) -> int:
-    return np.where(self.gameboard[:, column] == -1)[0][-1]
-
-  def placeAt(self, pos:int, plr:int):
+        return (self.getState(plr), 0, True)
+  def placeAt(self, pos, plr):
     col = self.getCol(pos)
-
-    if not -1 in self.gameboard: # game board is already full
+    if not -1 in self.gameboard:
         self.done = True
         return -100
-    
     if -1 in col and pos >= 0 and pos < self.cols:
-      coords = [self.findBottomFree(pos), pos]
-
+      coords = [-1, pos]
+      for i in range(self.rows):
+        row = self.rows - 1 - i
+        if col[row] == -1:
+          coords[0] = row
+          break
       self.gameboard[coords[0],coords[1]] = plr
-
+      self.state[0,coords[0]+self.rows*(plr+1), coords[1],0] = 1
+      self.state[1,coords[0]+(self.rows*3-self.rows*(plr+1)), coords[1],0] = 1
+      
+      self.state[0,coords[0], coords[1],0] = 0
+      self.state[1,coords[0], coords[1],0] = 0
+      self.state[0,coords[0]+self.rows*3, coords[1], 0] = 1
+      self.state[1,coords[0]+self.rows*3, coords[1], 0] = 1
       if(self.checkWin(plr, coords)):
         return plr
     else:
       print("NO")
       return -10 # Incorrect input
     return -1 ## Normal return
-  def getState(self, plr=None):
-    return self.gameboard.copy()
-
+  def getState(self, plr):
     if plr == 0:
        return self.state[0]
     return self.state[1]  
@@ -155,9 +123,6 @@ class game:
     return False
 
   def show(self):
-    """
-    Prints the gameboard to the screen
-    """
     board = []
     thing = []
     for i in range(self.rows):
